85f9692301ebead05086cfdd8aa06c32f021cfe7
DESKTOP-B4O2CCS\titus <titus.1@hotmail.com>
2022-07-08 15:17:37 +0200

    vue basic project

--- raspberry_setup.sh
+++ raspberry_setup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-sudo apt
+echo "upgrade packages"
+sudo apt-get upgrade
+echo "install git"
 sudo apt-get install git
+echo "install docker"
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
+echo "clone repo"
+echo "please use a personal access token in the password field"
+git clone https://github.com/jasperlg/AccessControlApp
+
+echo "build docker containers"
+docker compose build ./AccessControlApp
+echo "run docker containers"
+docker compose up ./AccessControlApp
