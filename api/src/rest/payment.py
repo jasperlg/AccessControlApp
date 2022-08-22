88e0cb8b0b556bf5d5b77de2e81d552acb6f1f54
jasperlg <louagejasper@gmail.com>
2022-08-22 17:29:12 +0200

    Payement - Payment

--- api/src/rest/payment.py
+++ api/src/rest/payment.py
@@ -1,51 +1,51 @@
 from flask import Blueprint, request
 from helpers.authMiddleware import tokenRequired
-from models.payment import paymentSchema, CreatepaymentsSchema, PaypaymentSchema
+from models.payment import PaymentSchema, CreatePaymentsSchema, PayPaymentSchema
 from helpers.successResponse import successResponse
-import data.payment as paymentService
+import data.payment as PaymentService
 
 paymentBP = Blueprint('payment', __name__)
 
 @paymentBP.route('/payment/<int:id>', methods=['GET'])
 @tokenRequired
-def getpayment(id: int):
-    schema = paymentSchema()
+def getPayment(id: int):
+    schema = PaymentSchema()
     
-    return schema.dumps(paymentService.getpayment(id))
+    return schema.dumps(PaymentService.getPayment(id))
 
 @paymentBP.route('/ticket/<int:ticket_id>/payment', methods=['GET'])
 @tokenRequired
-def getpaymentsOfTicket(ticket_id: int):
-    schema = paymentSchema(many=True)
+def getPaymentsOfTicket(ticket_id: int):
+    schema = PaymentSchema(many=True)
 
-    return schema.dumps(paymentService.getpaymentsOfTicket(ticket_id))
+    return schema.dumps(PaymentService.getPaymentsOfTicket(ticket_id))
 
 @paymentBP.route('/ticket/<int:ticket_id>/payment/add', methods=['POST'])
 @tokenRequired
-def addpaymentsOfTicket(ticket_id: int):
+def addPaymentsOfTicket(ticket_id: int):
     paymentsInput = request.form.to_dict()
     paymentsInput['ticket_id'] = ticket_id
 
-    schema = CreatepaymentsSchema()
-    newpayments = schema.load(paymentsInput)
+    schema = CreatePaymentsSchema()
+    newPayments = schema.load(paymentsInput)
 
-    paymentService.createpaymentsOfTicket(newpayments)
+    paymentService.createPaymentsOfTicket(newPayments)
 
     return successResponse()
 
 @paymentBP.route('/payment/<int:id>/pay', methods=['PUT'])
 @tokenRequired
-def paypayment(id: int):
-    schema = PaypaymentSchema()
+def payPayment(id: int):
+    schema = PayPaymentSchema()
     paymentInput = schema.load(request.form)
 
-    paymentService.paypayment(id, paymentInput.payed_amount)
+    paymentService.payPayment(id, paymentInput.payed_amount)
 
     return successResponse()
 
 @paymentBP.route('/payment/<int:id>', methods=['DELETE'])
 @tokenRequired
-def deletepayment(id: int):
-    paymentService.deletepayment(id)
+def deletePayment(id: int):
+    paymentService.deletePayment(id)
 
     return successResponse()
